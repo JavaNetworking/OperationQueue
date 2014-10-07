c426c541a655c74c283e965883d04e7c3a8f6f0a
Gard Sandholt <gard.sandholt@gmail.com>
2014-10-07 11:01:05 +0200

    Added OperationQueue test

--- library/src/test/java/com/operationqueue/OperationQueueTest.java
+++ library/src/test/java/com/operationqueue/OperationQueueTest.java
@@ -2,6 +2,9 @@ package com.operationqueue;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 
@@ -9,7 +12,36 @@ public class OperationQueueTest {
 	
 	@Test
 	public void testOperationQueue() {
-		assertTrue(true);
+		final CountDownLatch signal = new CountDownLatch(1);
+
+		// Setup OperationQueue with one base operation
+		OperationQueue operationQueue = new OperationQueue();
+		operationQueue.addOperation(new BaseOperation() {
+			@Override
+			public synchronized void execute() {
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			@Override
+			public synchronized void complete() {
+				signal.countDown();
+			}
+		});
+
+		// Test if empty
+		assertFalse(operationQueue.isEmpty());
+
+		// Wait for signal
+		try {
+            signal.await(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+		assertTrue(operationQueue.isEmpty());
 	}
 	
 }
