ec131ef94e8e45a31120ccbbe0a89ec63d75332d
Gard Sandholt <gard.sandholt@gmail.com>
2014-10-07 10:41:24 +0200

    Testing set up

---
+++ library/src/test/java/com/operationqueue/OperationQueueTest.java
@@ -0,0 +1,15 @@
+package com.operationqueue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class OperationQueueTest {
+	
+	@Test
+	public void testOperationQueue() {
+		assertTrue(true);
+	}
+	
+}
